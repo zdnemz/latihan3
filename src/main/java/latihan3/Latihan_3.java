88014ee597b76c92515cd27e884e06237ad21604
zdnemz <zidanemz69@gmail.com>
2025-06-11 16:02:01 +0800

    membuat class main

--- src/main/java/latihan3/Latihan_3.java
+++
@@ -1,8 +0,0 @@
-package latihan3;
-
-public class Latihan_3 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/latihan3/RumusBeraksi.java
@@ -0,0 +1,12 @@
+package latihan3;
+
+public class RumusBeraksi {
+
+    public static void main(String[] args) {
+        Rumus zidane = new Rumus();
+        
+        zidane.setLuasPersegi();
+        zidane.setLuasPersegiPanjang();
+        zidane.getHasil();
+    }
+}
